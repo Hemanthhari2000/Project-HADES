f9bf785db7800487e310a64ced976be5dd64e333
Hemanth <hemanthhari2000@gmail.com>
2021-05-27 11:46:54 +0530

    Add resize images

---
+++ resize_images/resize.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+image = cv2.imread("pi3.png", 1)
+# Loading the image
+ 
+# half = cv2.resize(image, (0, 0), fx = 0.1, fy = 0.1)
+# bigger = cv2.resize(image, (1050, 1610))
+ 
+stretch_near = cv2.resize(image, (512, 512),
+               interpolation = cv2.INTER_NEAREST)
+ 
+ 
+# Titles =["Original", "Half", "Bigger", "Interpolation Nearest"]
+# images =[image, half, bigger, stretch_near]
+# count = 4
+ 
+# for i in range(count):
+#     plt.subplot(2, 2, i + 1)
+#     plt.title(Titles[i])
+#     plt.imshow(images[i])
+ 
+# plt.show()
+
+cv2.imwrite("pi_3.png", stretch_near)
